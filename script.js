ae22bb30bd77a0011214bb32b6e8541f4283b7af
KenOginga <72943649+KenOginga@users.noreply.github.com>
2020-11-19 12:04:08 -0600

    added the querry request for both the listing and image endpoints

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
-$(".button").on("click", function(){
-    console.log("clicked")
 
-    var productName = $("#product-input").val().trim();
-    
-    var querryUrl = "https://cors-anywhere.herokuapp.com/https://openapi.etsy.com/v2/listings/active?api_key=xo2h2wgt4m30eqypx7c7pcfz&limit=10&tags=" + productName;
+function productSearch(){
 
-    $.ajax({
-        url: querryUrl,
-        method: "GET"
-    }).then(function(response){
-        console.log(response)
+
+    $(".button").on("click", function(){
+        console.log("clicked")
+
+        var productName = $("#product-input").val().trim();
+        var apiKey = "api_key=wswjour498ryj8w3bbrwiraw";
+        var querryUrl = "https://cors-anywhere.herokuapp.com/https://openapi.etsy.com/v2/listings/active?" + apiKey + "&limit=10&tags=" + productName;
+
+        $.ajax({
+            url: querryUrl,
+            method: "GET"
+        })
+        .then(function(response){
+            for ( var i = 0; i < response.results.length; i++){
+                var products = response.results;
+                
+                console.log(products[i]);
+                
+                var productCard = $("<div>").addClass("card col s12 m12 blue-grey darken-1 white-text");
+                
+                var productCardBody = $("<div>").addClass("card-body p-3");
+                
+                var productTitle = $("<span>").addClass("card-title").text(products[i].title);
+                var productId = $("<p>").addClass("card-text").text("Listing ID: " + products[i].listing_id);
+                var urlDiv = $("<div>").addClass("card-action");
+                var productUrl = $("<a>").attr("href", products[i].url).text("GET IT!!").addClass("get-it");
+                var productPrice = $("<h4>").addClass("card-text").text("Price: $" + products[i].price);
+
+                urlDiv.append(productUrl);
+                productCardBody.append(productTitle, productId, productPrice, urlDiv);
+                productCard.append(productCardBody);
+                var imageQuerryUrl = "https://cors-anywhere.herokuapp.com/https://openapi.etsy.com/v2/listings/:listing_id/images?api_key=wswjour498ryj8w3bbrwiraw&listing_id=" + products[i].listing_id;
+                $.ajax({
+                    url: imageQuerryUrl,
+                    method: "GET"
+        
+                }).then(function(imageResponse){
+                    for ( let j = 0; j < imageResponse.results.length; j++);
+                        var images = imageResponse.results;
+                        console.log(imageResponse.results[0]);
+                    
+                    var productImage = $("<div>").addClass("card-image");
+                    var imageElement = $("<img>").attr("src", imageResponse.results[0].url_570xN);
+                    productImage.append(imageElement);
+                    productCard.append(productImage);
+                    $(".productDisplay").append(productCard);   
+
+
+                })
+            }
+            
+        
     })
-  
 })
+} productSearch();
